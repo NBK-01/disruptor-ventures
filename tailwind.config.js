6c70308c2ffb9d82fccc5d91a5cfbffb7646f6fa
NBK-01 <nayefkanana03@gmail.com>
2023-01-30 18:24:02 +0300

    TW Setup landing page draft- no 3d

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      Main: ["Orbitron", "sans-serif"],
+      Sub: ["Inter", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        "dv-green": "#92995C",
+        "dv-dark": "#1E1E1E",
+        "dv-white": "#d1d1d1",
+      },
+    },
   },
   plugins: [],
 };
